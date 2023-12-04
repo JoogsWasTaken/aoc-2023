630ac2a33264c7e5c16e00c0f26b8ab4616de160
Maximilian <git@eulenbu.de>
2023-12-04 17:55:51 +0100

    Complete day 1, part 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,6 +1,31 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val digits = "0123456789"
+        var total = 0
+
+        for (line in input) {
+            val chars = line.toCharArray()
+
+            // search forwards
+            for (chr in chars) {
+                if (digits.contains(chr)) {
+                    total += chr.toString().toInt() * 10;
+                    break;
+                }
+            }
+
+            // search backwards
+            for (i in chars.indices.reversed()) {
+                val chr = chars[i];
+
+                if (digits.contains(chr)) {
+                    total += chr.toString().toInt();
+                    break;
+                }
+            }
+        }
+
+        return total
     }
 
     fun part2(input: List<String>): Int {
@@ -9,7 +34,7 @@ fun main() {
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 142)
 
     val input = readInput("Day01")
     part1(input).println()
