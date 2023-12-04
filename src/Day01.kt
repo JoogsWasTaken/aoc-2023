3368fce7e7a61e9fce35c029d0488bcb4766602a
Maximilian <git@eulenbu.de>
2023-12-04 21:05:56 +0100

    Complete day 1, part 2

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,3 +1,5 @@
+import kotlin.collections.mapOf as mapOf
+
 fun main() {
     fun part1(input: List<String>): Int {
         val digits = "0123456789"
@@ -29,12 +31,64 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val wordToNumber = mapOf(
+            "one" to 1,
+            "1" to 1,
+            "two" to 2,
+            "2" to 2,
+            "three" to 3,
+            "3" to 3,
+            "four" to 4,
+            "4" to 4,
+            "five" to 5,
+            "5" to 5,
+            "six" to 6,
+            "6" to 6,
+            "seven" to 7,
+            "7" to 7,
+            "eight" to 8,
+            "8" to 8,
+            "nine" to 9,
+            "9" to 9
+        )
+
+        var total = 0
+
+        for (line in input) {
+            var lowestIdx = line.length
+            var highestIdx = -1
+
+            var lowValue = 0
+            var highValue = 0
+
+            wordToNumber.forEach { (key, value) ->
+                val i = line.indexOf(key)
+
+                if (i == -1) {
+                    return@forEach
+                }
+
+                if (i < lowestIdx) {
+                    lowestIdx = i
+                    lowValue = value
+                }
+
+                val j = line.lastIndexOf(key)
+
+                if (j > highestIdx) {
+                    highestIdx = j
+                    highValue = value
+                }
+            }
+
+            total += lowValue * 10 + highValue
+        }
+
+        return total
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 142)
+    check(part1(readInput("Day01_part1_test")) == 142)
+    check(part2(readInput("Day01_part2_test")) == 281)
 
     val input = readInput("Day01")
     part1(input).println()
